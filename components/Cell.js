f3442ea5ac0667ad18b36b573f8292a902733981
Alexander Gugel <alexander.gugel@gmail.com>
2016-08-23 02:21:29 +0100

    Add components

--- components/Cell.js
+++ components/Cell.js
@@ -4,6 +4,15 @@ const React = require('react');
 
 class Cell extends React.Component {
   render () {
+    return (
+      <td className='cell'>
+        <input
+          value={this.props.cell}
+          onChange={this.props.onChange}
+          onFocus={this.props.onFocus}
+        />
+      </td>
+    );
   }
 }
 
--- components/Table.js
+++ components/Table.js
@@ -5,6 +5,22 @@ const Row = require('./Row');
 
 class Table extends React.Component {
   render () {
+    return (
+      <table className='table' onBlur={this.props.onBlur}>
+        <tbody>
+          {
+            this.props.table.map((row, rowIndex) =>
+              <Row
+                key={rowIndex}
+                row={row}
+                onChange={this.props.onChange.bind(null, rowIndex)}
+                onFocus={this.props.onFocus.bind(null, rowIndex)}
+              />
+            )
+          }
+        </tbody>
+      </table>
+    );
   }
 }
 
