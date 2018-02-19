f93f5d57185102e94fbec096094631625ca37fd7
kkterai <kkterai@gmail.com>
2018-02-18 22:46:08 -0800

    revising files to new structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-require('./test/index-test.js'); // Leave this in!
-
 import Spreadsheet from './components/Spreadsheet';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+require('./test/index-test.js'); // Leave this in!
+
 const $main = document.getElementById('main');
-ReactDOM.render(<Spreadsheet />, $main);
+ReactDOM.render(
+return <Spreadsheet />, $main);
