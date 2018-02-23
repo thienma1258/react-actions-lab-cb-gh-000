c9516b727ab6c25d57e82187a66472b1fa469e89
Matt Hough <matt@hough.tv>
2018-02-23 21:42:04 +1000

    Remove solution

--- components/Spreadsheet.js
+++ components/Spreadsheet.js
@@ -7,49 +7,27 @@ import Table from './Table';
 export default class Spreadsheet extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {
-      focused: null,
-      table: [
-        ['']
-      ]
-    };
-
-    this.handleAddColumn = actions.addColumn.bind(this);
-    this.handleAddRow = actions.addRow.bind(this);
-    this.handleRemoveColumn = actions.removeColumn.bind(this);
-    this.handleRemoveRow = actions.removeRow.bind(this);
-    this.handleChange = actions.changeCell.bind(this);
-    this.handleFocus = actions.focusCell.bind(this);
-    this.handleBlur = actions.blurCell.bind(this);
+    this.state = {};
   }
+
   render () {
     return (
       <div className='spreadsheet'>
         <h1>My little spreadsheet</h1>
 
         <div className='spreadsheet__buttons'>
-          <button onClick={this.handleAddColumn}>Add Column</button>
+          <button>Add Column</button>
           {' '}
-          <button onClick={this.handleAddRow}>Add Row</button>
+          <button>Add Row</button>
           {' '}
-          <button onClick={this.handleRemoveColumn}>Remove Column</button>
+          <button>Remove Column</button>
           {' '}
-          <button onClick={this.handleRemoveRow}>Remove Row</button>
+          <button>Remove Row</button>
         </div>
 
-        <Table
-          table={this.state.table}
-          onChange={this.handleChange}
-          onFocus={this.handleFocus}
-          onBlur={this.handleBlur}
-        />
+        <Table />
 
         <div className='spreadsheet__focused'>
-          {
-            this.state.focused
-            ? <strong>{this.state.focused[0]} — {this.state.focused[1]}</strong>
-            : null
-          }
         </div>
       </div>
     );
